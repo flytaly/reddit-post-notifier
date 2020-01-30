a258e1bf5bb1541b4657d4583931375a70304252
Vitaly Yerofeyevsky <flytaly@gmail.com>
2020-01-31 00:16:42 +0300

    Delete config.js

--- scripts/config.js
+++
@@ -1,5 +0,0 @@
-export const clientId = '';
-export const clientSecret = '';
-export const redirectUri = 'https://localhost/';
-// https://github.com/reddit-archive/reddit/wiki/api#rules
-export const userAgent = '';
