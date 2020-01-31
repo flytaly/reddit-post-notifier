0a2b7f4d1f7579924f71c56c0f2ad9ad6f1a254c
Vitaly Yerofeyevsky <flytaly@gmail.com>
2020-01-31 19:59:51 +0300

    Update config.js

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -3,13 +3,13 @@ const conf = {};
 if (TARGET === 'chrome') {
     conf.clientId = '';
     conf.clientSecret = '';
-    conf.redirectUri = 'https://localhost/';
+    conf.redirectUri = 'https://*.chromiumapp.org/';
     // https://github.com/reddit-archive/reddit/wiki/api#rules
     conf.userAgent = '';
 } else {
     conf.clientId = '';
     conf.clientSecret = '';
-    conf.redirectUri = 'https://*.chromiumapp.org/';
+    conf.redirectUri = 'https://localhost/';
     // https://github.com/reddit-archive/reddit/wiki/api#rules
     conf.userAgent = '';
 }
