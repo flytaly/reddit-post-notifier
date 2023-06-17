12de2cc7153b2389aa30aad9232f0563f7170652
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-06-17 18:03:32 +0300

    Port Manifest v2 -> v3

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,64 +1,61 @@
 import type { Manifest } from 'webextension-polyfill-ts';
 import pkg from '../package.json';
 import { isDev, port, target } from '../scripts/utils';
-import { DEV_SERVER, IS_DEV } from './constants';
 
 type ExtManifest = Manifest.WebExtensionManifest & { key?: string };
 
 const key =
     'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi+t5ySos/8pLUphc43Y+2Ee5ehEwX86iSRannZu9lOdZTtnD6X0eiVKUvr5y2Q9abSQQ1ZFmQkf4VkbHgff0rST+ihZuRTLvOBRLel/txvk9cbwQE3YOHChXzRqNCguPSkDHBVGLbXx5UIPdG6YMrnN6VGbbmUM7yW16g55uU6SjRZm/CL2r00f6kg/nOdKcXbAc8dwOOFCLeWfHdbNmcXvD6Ud9eeKmwm+XrLz0xgIFNOj+D4vqs/p3JwdWz49Ed3j0qNcM1MQvjI42P26kDXYG5w8O9If/WyaFd6/QgvmhBdNYjfVG878bWIxdTu+McmHtSG55WffGhShmoZL1/wIDAQAB';
 
+const bgScript = './dist/background.js';
+
 function browserSpecific() {
     const manifest: Partial<ExtManifest> = {};
     if (target === 'chrome') {
-        manifest.minimum_chrome_version = '86';
+        manifest.minimum_chrome_version = '105';
         manifest.key = key;
+        manifest.background = {
+            service_worker: bgScript,
+        };
     }
     if (target === 'firefox') {
         manifest.applications = {
             gecko: {
-                strict_min_version: '85.0',
+                strict_min_version: '109.0',
                 id: 'reddit-post-notifier@flytaly',
             },
+        };
+        manifest.background = {
+            scripts: [bgScript],
         };
     }
     return manifest;
 }
 
+const info: Partial<ExtManifest> = {
+    default_locale: 'en',
+    description: '__MSG_description__',
+    name: '__MSG_name__',
+    version: pkg.version,
+    homepage_url: 'https://github.com/flytaly/reddit-post-notifier',
+};
+
 export async function getManifest(): Promise<Manifest.WebExtensionManifest> {
-    // update this file to update this manifest.json
-    // can also be conditional based on your need
-    const permissions: string[] = [
-        'identity',
-        'storage',
-        'alarms',
-        'notifications',
-        'unlimitedStorage',
-        'https://*.reddit.com/*',
-        'https://*.redd.it/*',
-    ];
-    if (IS_DEV) {
-        permissions.push(`${DEV_SERVER}/*`);
-    }
+    /* if (IS_DEV) { */
+    /*     permissions.push(`${DEV_SERVER}/*`); */
+    /* } */
     return {
-        manifest_version: 2,
-        default_locale: 'en',
-        description: '__MSG_description__',
-        name: '__MSG_name__',
-        version: pkg.version,
-        homepage_url: 'https://github.com/flytaly/reddit-post-notifier',
+        manifest_version: 3,
+        ...info,
         ...browserSpecific(),
         icons: {
             '48': 'images/icon-48.png',
             '96': 'images/icon-96.png',
             '128': 'images/icon-128_chrome.png',
         },
-        permissions,
-        background: {
-            persistent: true,
-            page: 'background.html',
-        },
-        browser_action: {
+        permissions: ['identity', 'storage', 'alarms', 'notifications', 'unlimitedStorage'],
+        host_permissions: ['https://*.reddit.com/*', 'https://*.redd.it/*'],
+        action: {
             default_title: '__MSG_extension_title__',
             default_popup: 'dist/popup/index.html',
             default_icon: {
@@ -99,7 +96,11 @@ export async function getManifest(): Promise<Manifest.WebExtensionManifest> {
             open_in_tab: true,
         },
 
-        // this is required on dev for Vite script to load
-        content_security_policy: isDev ? `script-src 'self' http://localhost:${port}; object-src 'self'` : undefined,
+        content_security_policy: {
+            extension_pages: isDev
+                ? // this is required on dev for Vite script to load
+                  `script-src 'self' http://localhost:${port}; object-src 'self'`
+                : "script-src 'self'; object-src 'self'",
+        },
     };
 }
