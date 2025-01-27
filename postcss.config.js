3e2f1364de824cafb2b0f8736d1c42bf23cde397
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-01-27 14:36:09 +0300

    update: Tailwind v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 export default {
     plugins: {
-        'tailwindcss/nesting': {},
-        'tailwindcss': {},
+        '@tailwindcss/postcss': {},
         'autoprefixer': {},
-        'postcss-import': {},
     },
 };
